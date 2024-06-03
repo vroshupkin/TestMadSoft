da421ccfe1012c83f6c63f27f585d10007efe3be
Vadim Roschupkin <vroshupkin1@gmail.com>
2024-06-03 19:42:50 +0300

    Сделаны итераторы localStorage

--- src/utils/TestLocalStorage.ts
+++ src/utils/TestLocalStorage.ts
@@ -2,18 +2,20 @@ export class TestLocalStorage
 {
 	constructor(private testName: string){}
 
+	isTestKey = (key: string) => new RegExp(`${this.testName}_[0-9]+`).test(key);
+
 	clearTests()
 	{
-		for(let i = 0; i < localStorage.length; i++)
+		for(let ind = 0; ind < localStorage.length; ind++)
 		{
-			const key = localStorage.key(i);
+			const key = localStorage.key(ind);
 			if(!key) continue;
       
-			new RegExp(`test_${this.testName}[0-9]+`)
-				.test(key) ? localStorage.removeItem(key): '';
+			this.isTestKey(key) ? localStorage.removeItem(key): '';
 		}
 	}
 
+	
 	setOneOf(ind: number, value: number)
 	{
 		localStorage.setItem(`${this.testName}_${ind}`, value + '');
@@ -22,11 +24,77 @@ export class TestLocalStorage
 	{
 		localStorage.setItem(`${this.testName}_${ind}`, value.map(v => v ? '1' : '0').join(''));
 	}
-	
+	setInput(ind: number, value: string)
+	{
+		localStorage.setItem(`${this.testName}_${ind}`, value);
+	}
+
+	getMemory()
+	{
+		const keys = [...localStorageKeyIterator];
+		const entires = [...localStorageEntires];
+		console.log(keys);
+		console.log(entires)
+	}
+
 	compareTests() 
 	{
 
 	}
 
   
+}
+
+
+export const localStorageKeyIterator = {	
+	[Symbol.iterator]: function()
+	{		
+		let index = 0;
+		
+		return(
+			{
+				next: () =>
+				{					
+					const value = localStorage.key(index);
+					const done = index === localStorage.length;
+					
+					if(done)
+					{
+						return {done}
+					}
+					
+					index++;
+
+					return {done: done, value};
+				}
+			}
+		)
+	}
+}
+
+export const localStorageEntires = {	
+	[Symbol.iterator]: function()
+	{		
+		let index = 0;
+		
+		return(
+			{
+				next: () =>
+				{					
+					const key = localStorage.key(index);
+					const value = [key, key != null ? localStorage.getItem(key) : ''];
+					const done = index === localStorage.length;
+					
+					if(done)
+					{
+						return {done}
+					}
+					
+					index++;
+
+					return {done: done, value};
+				}
+			}
+		)
+	}
 }
