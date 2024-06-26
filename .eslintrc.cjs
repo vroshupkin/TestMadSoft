2859ca3314011e64f152cf68f5c8273357778630
Vadim Roschupkin <vroshupkin1@gmail.com>
2024-06-26 09:33:00 +0300

    Добавлены правила

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,8 @@ module.exports = {
      "@typescript-eslint/ban-ts-comment": "warn",
      "@typescript-eslint/no-unused-vars": "warn",
      "indent": [2, "tab"],
-     "no-mixed-spaces-and-tabs": "warn"
+     "no-mixed-spaces-and-tabs": "warn",
+     "@typescript-eslint/ban-types": "warn"
   },
+  
 }
